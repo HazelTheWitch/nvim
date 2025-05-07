8def58f8123b6958a97a2e218675fd29ec00186d
Hazel Rella <hazelrella11@gmail.com>
2025-05-07 12:10:17 -0700

    feat: tailwind support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,17 @@ return {
         },
     },
     {
+  "luckasRanarison/tailwind-tools.nvim",
+        name = "tailwind-tools",
+        build = ":UpdateRemotePlugins",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {}
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
