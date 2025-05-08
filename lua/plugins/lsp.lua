2ce85ab04cdf45a1b41dce6d198931045a0390ff
Hazel Rella <hazelrella11@gmail.com>
2025-05-07 20:51:51 -0700

    feat: astro emmet

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -50,6 +50,7 @@ return {
                     "svelte",
                     "wgsl",
                     "zig",
+                    "astro",
                 },
                 highlight = { enable = true },
             })
