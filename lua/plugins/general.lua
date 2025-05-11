d62bb0561de2e49e45f139049b3239c94d8e78b5
Hazel Rella <hazelrella11@gmail.com>
2025-05-10 18:57:14 -0700

    fix: remove duplicate which-key

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,9 +1,5 @@
 return {
     {
-        "folke/which-key.nvim",
-        opts = {},
-    },
-    {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
